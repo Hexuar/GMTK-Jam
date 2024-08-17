db3be3b4623975ee90fe3954fcd623329d5586d6
Hexuar <hexuar@gmail.com>
2024-08-17 22:28:11 +0200

    Jetpack Retexture

--- scripts/player.gd
+++ scripts/player.gd
@@ -40,7 +40,27 @@ func handle_movement(delta):
 		velocity += get_gravity() * delta
 	
 	
-	## Coyote Time
+	## Boost
+	if dashTime > -dashCooldown:
+		dashTime -= delta
+	if dashTime < 0.0 and is_on_floor():
+		dashTime = -dashCooldown
+	if dashTime > 0.0 :
+		_speed = move_toward(_speed, speed * dashModifier, 100)
+	else:
+		_speed = speed
+	if Input.is_action_just_pressed("Dash") and has_node("Wheel") and dashTime <= -dashCooldown:
+		dashTime = dashLength
+	
+	
+	## Input
+	var direction := Input.get_axis("Left", "Right")
+	if direction:
+		velocity.x = direction * _speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, _speed*friction)
+		
+		## Coyote Time
 	if floorCooldown > 0.0:
 		floorCooldown -= delta
 	if is_on_floor():
@@ -53,36 +73,14 @@ func handle_movement(delta):
 	
 	
 	## Jetpack
-	Jetpack.frame = 0
+	Jetpack.frame = direction+1
 	if Input.is_action_pressed("Jump") and canJump and has_node("Jetpack") and !floorCooldown > 0.0 and position.y > -256:
 		velocity.y = jumpVelocity
-		Jetpack.frame = 1
+		Jetpack.frame += 3
 	
-	
-	## Boost
-	if dashTime > -dashCooldown:
-		dashTime -= delta
-	if dashTime < 0.0 and is_on_floor():
-		dashTime = -dashCooldown
-	if dashTime > 0.0 :
-		_speed = move_toward(_speed, speed * dashModifier, 100)
-	else:
-		_speed = speed
-	if Input.is_action_just_pressed("Dash") and has_node("Wheel") and dashTime <= -dashCooldown:
-		dashTime = dashLength	
-	
-	
-	## Input
-	var direction := Input.get_axis("Left", "Right")
-	if direction:
-		velocity.x = direction * _speed
-	else:
-		velocity.x = move_toward(velocity.x, 0, _speed*friction)
-
 	
 	## Animation
 	Bot.frame = direction+1
 	Wheel.rotation += velocity.x / 1000
-	
 	
 	move_and_slide()
