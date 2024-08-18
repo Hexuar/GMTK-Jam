0db10a462176d41538e549bede65b50a9a842c76
Hexuar <hexuar@gmail.com>
2024-08-18 18:12:18 +0200

    Boulder Cave

--- scripts/boulder.gd
+++ scripts/boulder.gd
@@ -1,14 +1,19 @@
 extends RigidBody2D
 
 var spawnLocation : Vector2
+@export var lifeTime : int = 2
+
+var time
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	spawnLocation = position * owner.scale
+	reset()
 
 func reset():
+	time = lifeTime
 	linear_velocity = Vector2()
-	angular_velocity = randf_range(0,PI)
+	angular_velocity = 10.0
 	PhysicsServer2D.body_set_state(
 		get_rid(),
 		PhysicsServer2D.BODY_STATE_TRANSFORM,
@@ -17,5 +22,6 @@ func reset():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	if position.y > 1000:
+	time -= delta
+	if time < 0.0:
 		reset()
