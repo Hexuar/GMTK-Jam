5e4138071a17da4c2c4ae64d2c66b6fd09ec4338
Hexuar <hexuar@gmail.com>
2024-08-19 00:37:47 +0200

    Warn Fixes

--- scripts/stats.gd
+++ scripts/stats.gd
@@ -5,7 +5,7 @@ extends TextureRect
 @onready var DeathsText = $Deaths
 
 
-func _process(delta: float) -> void:
-	var time = Time.get_time_dict_from_unix_time(Time.get_ticks_msec() / 1000)
+func _process(_delta: float) -> void:
+	var time = Time.get_time_dict_from_unix_time(int(Time.get_ticks_msec() / 1000.0))
 	TimeText.text = "[color=\"black\"]Time: %s:%s[/color]" % [time.minute,time.second]
 	DeathsText.text = "[color=\"black\"]Deaths: %s[/color]" % Main.deaths 
